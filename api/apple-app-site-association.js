670133b64a083f9a00fd9d9e73bcfabd500b9efa
Kaibin <kaibinding@gmail.com>
2026-05-04 19:03:14 +0800

    fix: revert AASA Team ID to H58Z24BP7C

--- api/apple-app-site-association.js
+++ api/apple-app-site-association.js
@@ -5,7 +5,7 @@ module.exports = (req, res) => {
         apps: [],
         details: [
           {
-            appID: 'S7NV8C9UQ2.com.markpie.app',
+            appID: 'H58Z24BP7C.com.markpie.app',
             paths: ['/wechat/*'],
           },
         ],
